27c5092467ed86d088366214be2bf8766359894d
David Divilly <d.divilly@gmail.com>
2017-08-31 13:36:23 +0100

    added min temp throw and tests

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,22 +6,36 @@ describe('Themostat', function(){
     thermostat = new Thermostat();
   });
 
-describe('when initialized', function(){
+  describe('when initialized', function(){
 
-  it('should start with a defualt value of 20', function(){
-    expect(thermostat.temp).toEqual(STARTING_TEMP);
+    it('should start with a defualt value of 20', function(){
+      expect(thermostat.temp).toEqual(STARTING_TEMP);
+    });
   });
 
-  it ('can increase temperature with up function', function() {
-    thermostat.up(5)
-    expect(thermostat.temp).toEqual(STARTING_TEMP + 5)
+
+  describe('turning the temp up', function(){
+
+    it ('can increase temperature with up function', function() {
+      thermostat.up(5)
+      expect(thermostat.temp).toEqual(STARTING_TEMP + 5)
+    })
+
   })
 
-  it ('can reduce temperature with down function', function() {
-    thermostat.down(5)
-    expect(thermostat.temp).toEqual(STARTING_TEMP - 5)
-  })
+  describe('turning the temp down', function(){
 
-});
+    it ('can reduce temperature with down function', function() {
+      thermostat.down(5)
+      expect(thermostat.temp).toEqual(STARTING_TEMP - 5)
+    })
+    
 
+    it ('should not go below 10 degrees', function(){
+      thermostat.setTemp(MIN_TEMP)
+      expect(function(){
+        thermostat.down(1)
+      }).toThrow("10 is the lowest!");
+    })
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,14 +1,23 @@
 STARTING_TEMP = 20
+MIN_TEMP = 10
 
 var Thermostat = function(){
   this.temp = STARTING_TEMP
 
 };
 
-Thermostat.prototype.up = function (value) {
+Thermostat.prototype.up = function (value = 1) {
   this.temp += value
 };
 
-Thermostat.prototype.down = function (value) {
+Thermostat.prototype.down = function (value = 1) {
+  if (this.temp == MIN_TEMP) {
+    throw "10 is the lowest!"
+  } else {
   this.temp -= value
+  }
+};
+
+Thermostat.prototype.setTemp = function(value){
+  this.temp = value
 };
